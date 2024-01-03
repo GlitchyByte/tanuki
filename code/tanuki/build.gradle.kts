33d748bfad49997111aa89fa0a92c338b20bc09f
Luis Mejia <lmejia@gmail.com>
2024-01-03 14:07:09 -0800

    Added native image creation.

--- code/tanuki/build.gradle.kts
+++ code/tanuki/build.gradle.kts
@@ -1,9 +1,9 @@
-// Copyright 2023 GlitchyByte
+// Copyright 2023-2024 GlitchyByte
 // SPDX-License-Identifier: Apache-2.0
 
 plugins {
     id("glitchybyte.java-application-conventions")
-//    id("org.graalvm.buildtools.native") version "0.9.28"
+    id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
 dependencies {
@@ -18,12 +18,20 @@ application {
     mainClass.set("com.glitchybyte.tanuki.App")
 }
 
-//graalvmNative {
-//    binaries {
-//        named("main") {
-//            buildArgs.add("-O3")
-////            buildArgs.add("--compress")
-////            buildArgs.add("--strip")
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("tanuki")
+            buildArgs.add("-march=native")
+            buildArgs.add("-O3")
+            buildArgs.add("-H:+StrictImageHeap")
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.matching("Oracle"))
+            })
+        }
+//        all {
+//            buildArgs.add("--verbose")
 //        }
-//    }
-//}
+    }
+}
