73d52ac0a5fb56f0d1deebae3ef63643b24f7e17
Luis Mejia <lmejia@gmail.com>
2024-03-31 01:13:39 -0700

    Force UTF-8 locale.

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,10 +1,11 @@
-// Copyright 2023 GlitchyByte
+// Copyright 2023-2024 GlitchyByte
 // SPDX-License-Identifier: Apache-2.0
 
 #include "gb.h"
 #include "App.h"
 
 int main(int const argc, char const* argv[]) {
+    setlocale(LC_ALL, "en_US.UTF-8");
     auto const& args = gb::strings::createVectorStringViewFromCArray(argc, argv);
     return App::run(args);
 }
